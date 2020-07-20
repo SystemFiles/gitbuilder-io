c1d2b55bfb230e464a5b39a77068936ae537d574
Ben Sykes <ben@sykesdev.ca>
2020-07-20 16:24:55 -0400

    local readme and .gitignore automation complete

--- app.js
+++ app.js
@@ -23,7 +23,39 @@ const run = async () => {
 	if (!files.directoryExists(`${files.getProjectDirectoryIfExists(projectDetails.project_name)}/.gitignore`)) {
 		files.touch(`${files.getProjectDirectoryIfExists(projectDetails.project_name)}/.gitignore`)
 	}
-	await files.writeGitIgnoreToProject(projectDetails.project_name, respAskIgnore.gitignore_files)
+
+	await new Listr(
+		[
+			{
+				title : 'Write .gitignore to project directory',
+				task  : async () =>
+					await files
+						.writeGitIgnoreToProject(projectDetails.project_name, respAskIgnore.gitignore_files)
+						.catch((err) => {
+							throw new Error(`Problem writing .gitignore to project. ${err}`)
+						})
+			},
+			{
+				title : 'Copy selected README template to project directory',
+				task  : async () =>
+					await files.copyReadmeTemplateToProjects(projectDetails.project_name, projectDetails.readme_style),
+				skip  : () => projectDetails.include_readme
+			},
+			{
+				title : 'Copy project structure template',
+				task  : async () => await files.copyProjectTemplate(),
+				skip  : () => true
+			},
+			{
+				title : 'Initialize project as Git project',
+				task  : async () => await repo.createLocalRepo(),
+				skip  : () => true
+			}
+		],
+		{ concurrent: true }
+	)
+		.run()
+		.then(() => console.log(chalk.greenBright('Successfully created local project repository!')))
 
 	// Start building the remote repository
 	console.log(chalk.bold(`\n--------- [ Starting build for ${projectDetails.project_name} ] ---------\n`))
@@ -36,11 +68,35 @@ const run = async () => {
 					.catch((err) => {
 						throw new Error(`Problem creating remote repository. ${err}`)
 					})
-			}
+			},
+			skip  : () => true
+		},
+		{
+			title : 'Attach local project repository to remote',
+			task  : async () => {
+				await repo.attachToRemote(projectDetails.https_url).catch((err) => {
+					throw new Error(`Problem attaching local repository to remote. ${err}`)
+				})
+			},
+			skip  : () => true
 		}
-	]).run()
+	])
+		.run()
+		.then(() =>
+			console.log(chalk.greenBright('Successfully created remote repository and connected to local git project!'))
+		)
+
+	return projectDetails.https_url
 }
 
-run().catch((err) => {
-	console.log(chalk.red(`Uncaught Error => ${err}`))
-})
+run()
+	.then((remoteURL) => {
+		console.log(
+			chalk.cyanBright(
+				`Project has been created successfully! It is available on Github at ${chalk.yellow(remoteURL)}`
+			)
+		)
+	})
+	.catch((err) => {
+		console.log(chalk.red(`Uncaught Error => ${err}`))
+	})
